8184c809b02fa21bd264b724bfb2de807f273dcd
MashiroQun <masakinoel@hotmail.com>
2026-04-13 07:26:42 +0900

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'med-mgr-v1';
+const CACHE_NAME = 'gennyaku-manager-v2'; 
+
 const ASSETS = [
   './',
   './index.html',
-  'https://mashiroqun.github.io/-/icon.png'
+  './manifest.json',
+  'https://mashiroqun.github.io/-/icon.png',
+  './icon.png?v=2'
 ];
 
-// インストール時にファイルをキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -14,11 +16,31 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// オフライン時はキャッシュから応答
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
